67f66992d5e7c8d1598e4af5ab1611026ea6f3d9
lvalkering <l.j.valkering@student.tudelft.nl>
2025-03-18 09:45:24 +0100

    Polo and I created some amazing code to find the ring radius based on the particle with the highest vorticity. Kind regards :)

--- Maximum vorticity.py
+++ Maximum vorticity.py
@@ -3,21 +3,21 @@ import numpy as np
 import ReadData as rd
 import math as m
 
-def radius_from_max_vorticity(filename):
+def RadiusFromMaxVorticity(filename):
     # get starting conditions
     X, Y, Z, U, V, W, Wx, Wy, Wz, Radius, Group_ID, Viscosity, Viscosity_t = rd.readVortexRingInstance('dataset/Vortex_Ring_DNS_Re7500_0000.vtp')
     ringRadius0, ringPos0 = rd.getRingPosRadius(X,Y,Z,Wx,Wy,Wz)
     timeStamps = np.arange(25,1575,25)
     Velocity = np.ones(len(timeStamps))
-    W_magnitude = np.sqrt(Wy**2 + Wz**2)
+    WMagnitude = np.sqrt(Wy**2 + Wz**2)
 
     #find the maximum and its location
-    max_W = np.max(W_magnitude)
-    idx_max_W = np.where(W_magnitude == max_W)
+    MaxW = np.max(WMagnitude)
+    IdxMaxW = np.where(WMagnitude == MaxW)
 
     #Find the radius using the coordinates
-    X_max_W, Y_max_W, Z_max_W = X[idx_max_W], Y[idx_max_W], Z[idx_max_W]
-    ring_radius0 = np.sqrt(Y[idx_max_W]**2 + Z[idx_max_W]**2)
+    XMaxW, YMaxW, ZMaxW = X[IdxMaxW], Y[IdxMaxW], Z[IdxMaxW]
+    ringRadius0 = np.sqrt(Y[IdxMaxW]**2 + Z[IdxMaxW]**2)
     
 
     for i in range(len(timeStamps)):
@@ -28,20 +28,22 @@ def radius_from_max_vorticity(filename):
         X, Y, Z, U, V, W, Wx, Wy, Wz, Radius, Group_ID, Viscosity, Viscosity_t = rd.readVortexRingInstance('dataset/Vortex_Ring_DNS_Re7500_' + zeros[4-len(stringtime)] + stringtime + '.vtp')
 
         #take magnitude of only y and z components
-        W_magnitude = np.sqrt(Wy**2 + Wz**2)
+        WMagnitude = np.sqrt(Wy**2 + Wz**2)
 
         #find the maximum and its location
-        max_W = np.max(W_magnitude)
-        idx_max_W = np.where(W_magnitude == max_W)
+        MaxW = np.max(WMagnitude)
+        IdxMaxW = np.where(WMagnitude == MaxW)
 
         #Find the radius using the coordinates
-        X_max_W, Y_max_W, Z_max_W = X[idx_max_W], Y[idx_max_W], Z[idx_max_W]
-        ring_radius = np.sqrt(Y[idx_max_W]**2 + Z[idx_max_W]**2)
+        XMaxW, YMaxW, ZMaxW= X[IdxMaxW], Y[IdxMaxW], Z[IdxMaxW]
+        ringRadius = np.sqrt(Y[IdxMaxW]**2 + Z[IdxMaxW]**2)
         ringRadius, ringPos = rd.getRingPosRadius(X, Y, Z, Wx, Wy, Wz)
         Velocity[i] = m.sqrt((ringPos[0] - ringPos0[0])**2 + (ringPos[1] - ringPos0[1])**2 + (ringPos[2] - ringPos0[2])**2)/timeStamps[i]*1000
         ringPos0 = ringPos
 
-    return ring_radius, X_max_W, Y_max_W, Z_max_W
+    return ringRadius, XMaxW, YMaxW, ZMaxW
 
 
-print(radius_from_max_vorticity('dataset\Vortex_Ring_DNS_Re7500_0000.vtp'))
+print(RadiusFromMaxVorticity('dataset\Vortex_Ring_DNS_Re7500_0000.vtp'))
+
+
---
+++ Saffman Model.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+#Extended Saffman velocity model:
+u = Gamma/(4*np.pi*R)(np.log)
+
+
+(np.log(4R/np.sqrt(nu*t))-0.558-3.6716*nu*t/R**2)
