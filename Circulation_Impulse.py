64882250507c72e14511648c450d2325a8b6753e
JesperSmits <jespersmits2004@gmail.com>
2025-03-25 12:32:04 +0100

    Update Circulation_Impulse.py

---
+++ Circulation_Impulse.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math as m
+
+
+
+def LinearImpuls(CirculationArray, ViscousityArea):
+
+    TimeStamps = np.arange(0,1575,25) / 1000
+    LArray = (2*np.array(ViscousityArea)*TimeStamps)**0.5
+    theta = 1 / LArray
+    R0 = 1
+
+    gamma0_array = np.array(CirculationArray) / (1 - np.exp(-(theta**2) / 2))
+
+
+    Impulses = m.pi * gamma0_array * (R0**2)  
+    
+    return Impulses 
+
+Array1 = [0,1,13,2,13]
+Array2 = [0,1,2,3,4]
+
+print(LinearImpuls(Array1, Array2))
