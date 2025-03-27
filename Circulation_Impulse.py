f86bdfd095da8da01e21b94bfb783189f1f20abc
JesperSmits <jespersmits2004@gmail.com>
2025-03-27 16:14:47 +0100

    Linear Impuls Changes

--- Circulation_Impulse.py
+++ Circulation_Impulse.py
@@ -3,10 +3,35 @@ import math as m
 
 
 
-def LinearImpulse(CirculationArray, ViscousityArea):
+#===Linear Impuls===
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#===Linear Impulse function, For the comparison====
+#This function calculates the Linear Impuls for every time step. 
+#The input is a array of the Circulation values of the hole vortex ring for every time step
+#and the viscousity is constant so can just plug that in the function command.
+
+Viscousity = 0.00013333333333333334
+
+def LinearImpuls(CirculationArray, Viscousity):
 
     TimeStamps = np.arange(0,1575,25) / 1000
-    LArray = (2*np.array(ViscousityArea)*TimeStamps)**0.5
+    LArray = (2*np.array(Viscousity)*TimeStamps)**0.5
     theta = 1 / LArray
 
     gamma0_array = np.array(CirculationArray) / (1 - np.exp(-(theta**2) / 2))
@@ -15,8 +40,3 @@ def LinearImpulse(CirculationArray, ViscousityArea):
     Impulses = m.pi * gamma0_array * (R0**2)  
     
     return Impulses 
-
-Array1 = [0,1,13,2,13]
-Array2 = [0,1,2,3,4]
-
-print(LinearImpuls(Array1, Array2))
