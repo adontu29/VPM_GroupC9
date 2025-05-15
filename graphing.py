a0a09dfc018708bb71a03a54e890be71949359ff
Yu-Jeffrey <Y.Xu-57@student.tudelft.nl>
2025-05-15 22:28:51 +0800

    1

--- graphing.py
+++ graphing.py
@@ -8,6 +8,5 @@ def getGraph(time, data, label, ylimit,colour='r'):
     plt.plot(time, data, 'k-')
     plt.ylim([0, ylimit])
     plt.legend()
-getGraph(np.linspace(0,10),np.linspace(0,10),label='asdas', colour='b', ylimit = 8)
-plt.show()
+
 
