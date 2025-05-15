e31e4d6e08a8b7916b72122d0e0ca777d18e4ca7
MaxR-TUD <maximilienroels@gmail.com>
2025-05-15 12:30:26 +0200

    plotting function

---
+++ graphing.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+from matplotlib import axis as ax
+import numpy as np
+
+
+def getGraph(time, data, label, ylimit,colour='r'):
+    plt.scatter(time, data, edgecolors='k', color=colour, label=label)
+    plt.plot(time, data, 'k-')
+    plt.ylim([0, ylimit])
+    plt.legend()
+getGraph(np.linspace(0,10),np.linspace(0,10),label='asdas', colour='b', ylimit = 8)
+plt.show()
+
