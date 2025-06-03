5feb879caf7a8e7d9438ed132e921d26c70ec2d7
MaxR-TUD <maximilienroels@gmail.com>
2025-06-03 11:59:21 +0200

    plotting function

--- graphing.py
+++ graphing.py
@@ -1,13 +1,21 @@
 from matplotlib import pyplot as plt
 from matplotlib import axis as ax
 import numpy as np
-
+import pandas as pd
 
 def getGraph(time, data, label, ylimit,colour='r'):
     plt.scatter(time, data, edgecolors='k', color=colour, label=label)
     plt.plot(time, data, 'k-')
     plt.ylim([0, ylimit])
     plt.legend()
-getGraph(np.linspace(0,10),np.linspace(0,10),label='asdas', colour='b', ylimit = 8)
-plt.show()
 
+
+data1= pd.read_csv('CSV-GROUPC9-enstrophy-stuff/Vorticity 6080.csv', sep=';', decimal=',').to_numpy()
+
+getGraph(data1[:,0], data1[:,1], label='Vorticity', ylimit=30, colour='r')
+
+plt.ylabel(r'$\omega^*$')
+plt.xlabel(r'$T$')
+
+
+plt.show()
